839c3d5b156df16b59e6d28dd7db23cac952cfd9
sandro.moesch1 <sandro.moesch@stud.hslu.ch>
2024-11-10 16:43:38 +0100

    Localhost doesnt work, we need to refer to the docker image name

--- common/src/main/java/ch/hslu/swda/common/config/ApplicationConfig.java
+++ common/src/main/java/ch/hslu/swda/common/config/ApplicationConfig.java
@@ -3,7 +3,7 @@ package ch.hslu.swda.common.config;
 import java.util.Optional;
 
 public class ApplicationConfig {
-    private static final String DEFAULT_CONNECTION = "mongodb://localhost:27017";
+    private static final String DEFAULT_CONNECTION = "mongodb://mongodb:27017";
     private static final String DEFAULT_DATABASE = "data";
 
     public static String getConnectionString() {
