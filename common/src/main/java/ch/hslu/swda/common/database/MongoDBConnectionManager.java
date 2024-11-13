9a98eb0346121c127355fcd7b42bec6f6439e100
Stocker Sven <sven.stocker@stud.hslu.ch>
2024-11-13 20:43:41 +0100

    try to add OffsetDateTimeCode as CodecRegistry

--- common/src/main/java/ch/hslu/swda/common/database/MongoDBConnectionManager.java
+++ common/src/main/java/ch/hslu/swda/common/database/MongoDBConnectionManager.java
@@ -5,8 +5,11 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 import org.bson.UuidRepresentation;
+import org.bson.codecs.Codec;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
+
+import java.time.OffsetDateTime;
 
 import static org.bson.codecs.configuration.CodecRegistries.*;
 
@@ -18,8 +21,23 @@ public class MongoDBConnectionManager {
     private MongoDBConnectionManager(String connectionString, String databaseName) {
         CodecRegistry pojoCodecRegistry = fromRegistries(
                 MongoClientSettings.getDefaultCodecRegistry(),
-//                fromProviders(PojoCodecProvider.builder().automatic(true).build()),
-                fromCodecs(new OffsetDateTimeCodec())
+                fromProviders(PojoCodecProvider.builder().automatic(true).build()),
+                fromRegistries(new CodecRegistry() {
+                    @Override
+                    public <T> Codec<T> get(Class<T> aClass, CodecRegistry codecRegistry) {
+                        if (aClass == OffsetDateTime.class) {
+                            return (Codec<T>) new OffsetDateTimeCodec();
+                        } else {
+                            return codecRegistry.get(aClass);
+
+                        }
+                    }
+
+                    @Override
+                    public <T> Codec<T> get(Class<T> aClass) {
+                        return (Codec<T>) new OffsetDateTimeCodec();
+                    }
+                })
         );
 
         MongoClientSettings settings = MongoClientSettings.builder()
