39558fb62be14fb778f9d0acc6283e473d52c75f
Stocker Sven <sven.stocker@stud.hslu.ch>
2024-11-13 14:13:18 +0100

    add findAll method in OrderDAO

--- common/src/main/java/ch/hslu/swda/common/database/OrderDAO.java
+++ common/src/main/java/ch/hslu/swda/common/database/OrderDAO.java
@@ -1,9 +1,10 @@
 package ch.hslu.swda.common.database;
 
-import ch.hslu.swda.common.entities.Customer;
 import ch.hslu.swda.common.entities.Order;
 import com.mongodb.client.MongoDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static com.mongodb.client.model.Filters.eq;
@@ -15,5 +16,9 @@ public class OrderDAO extends GenericDAO<Order> {
 
     public Order findByUUID(UUID uuid) {
         return collection.find(eq("_id", uuid)).first();
+    }
+
+    public List<Order> findAll() {
+        return collection.find().into(new ArrayList<>());
     }
 }
