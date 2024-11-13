5c751bf972b734dbd662386cac22192dbc340e57
Stocker Sven <sven.stocker@stud.hslu.ch>
2024-11-13 14:52:25 +0100

    fix person entity

--- common/src/test/java/ch/hslu/swda/common/entities/PersonTest.java
+++ common/src/test/java/ch/hslu/swda/common/entities/PersonTest.java
@@ -1,24 +1,28 @@
 package ch.hslu.swda.common.entities;
 
-import ch.hslu.swda.common.entities.Person;
 import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
 
 class PersonTest {
 
     @Test
     void testCreatePerson() {
-        Person person = new Person(1, "Hans", "Muster");
-        Assertions.assertThat(person.getId()).isEqualTo(1);
+        UUID id = UUID.randomUUID();
+        Person person = new Person(id, "Hans", "Muster");
+        Assertions.assertThat(person.getId()).isEqualTo(id);
         Assertions.assertThat(person.getFirstName()).isEqualTo("Hans");
-        Assertions.assertThat(person.getLastName()).isEqualTo("Muster");
+        Assertions.assertThat(person.getFamilyName()).isEqualTo("Muster");
     }
 
     @Test
     void testEqualsContract() {
         EqualsVerifier.forClass(Person.class)
                 .withOnlyTheseFields("id")
+                .suppress(Warning.NONFINAL_FIELDS)
                 .verify();
     }
 
