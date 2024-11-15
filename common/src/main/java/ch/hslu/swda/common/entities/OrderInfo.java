e783db7105784851a1bab4e9765d82bfbb544a42
David Dias Horta <david.diashorta@stud.hslu.ch>
2024-11-15 17:45:20 +0100

    OrderInfo: add deliveryDate

--- common/src/main/java/ch/hslu/swda/common/entities/OrderInfo.java
+++ common/src/main/java/ch/hslu/swda/common/entities/OrderInfo.java
@@ -1,27 +1,30 @@
 package ch.hslu.swda.common.entities;
 
+import java.time.LocalDate;
 import java.util.Objects;
 import java.util.UUID;
 
-public class OrderInfo {
+public final class OrderInfo {
     private final UUID trackingId;
     private final int productId;
     private final OrderItemStatus status;
     private final int quantity;
+    private final LocalDate deliveryDate;
 
-    public OrderInfo(UUID trackingId, int productId, OrderItemStatus status, int quantity) {
+    public OrderInfo(UUID trackingId, int productId, OrderItemStatus status, int quantity, LocalDate deliveryDate) {
         this.trackingId = trackingId;
         this.productId = productId;
         this.status = status;
         this.quantity = quantity;
+        this.deliveryDate = deliveryDate;
     }
 
-    public OrderInfo(int productId, OrderItemStatus status, int quantity) {
-        this(UUID.randomUUID(), productId, status, quantity);
+    public OrderInfo(int productId, OrderItemStatus status, int quantity, LocalDate deliveryDate) {
+        this(UUID.randomUUID(), productId, status, quantity, deliveryDate);
     }
 
     public OrderInfo() {
-        this(0, OrderItemStatus.NOT_FOUND, 0);
+        this(0, OrderItemStatus.NOT_FOUND, 0, LocalDate.now());
     }
 
     public UUID getTrackingId() {
@@ -40,6 +43,10 @@ public class OrderInfo {
         return quantity;
     }
 
+    public LocalDate getDeliveryDate() {
+        return deliveryDate;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -47,12 +54,13 @@ public class OrderInfo {
         return productId == that.productId &&
                 Objects.equals(trackingId, that.trackingId) &&
                 status == that.status &&
-                quantity == that.quantity;
+                quantity == that.quantity &&
+                Objects.equals(deliveryDate, that.deliveryDate);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(trackingId, productId, status, quantity);
+        return Objects.hash(trackingId, productId, status, quantity, deliveryDate);
     }
 
     public String toString() {
@@ -61,6 +69,7 @@ public class OrderInfo {
                 ", productId=" + productId +
                 ", status=" + status +
                 ", quantity=" + quantity +
+                ", deliveryDate=" + deliveryDate +
                 '}';
     }
 }
