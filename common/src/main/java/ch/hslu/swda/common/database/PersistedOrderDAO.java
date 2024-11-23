bf3980218dda12c8221c98cce6743e46c1a479cd
Stocker Sven <sven.stocker@stud.hslu.ch>
2024-11-23 18:02:16 +0100

    add findByUUID method to PersistedOrderDAO

--- common/src/main/java/ch/hslu/swda/common/database/PersistedOrderDAO.java
+++ common/src/main/java/ch/hslu/swda/common/database/PersistedOrderDAO.java
@@ -3,8 +3,16 @@ package ch.hslu.swda.common.database;
 import ch.hslu.swda.common.entities.PersistedOrder;
 import com.mongodb.client.MongoDatabase;
 
+import java.util.UUID;
+
+import static com.mongodb.client.model.Filters.eq;
+
 public class PersistedOrderDAO extends GenericDAO<PersistedOrder> {
     public PersistedOrderDAO(MongoDatabase database) {
         super(database, "orders", PersistedOrder.class);
+    }
+
+    public PersistedOrder findByUUID(UUID orderId) {
+        return collection.find(eq("orderId", orderId)).first();
     }
 }
