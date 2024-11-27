1d99affbe7fd3953df3ee6f08e624fff132eb5df
sandro.moesch1 <sandro.moesch@stud.hslu.ch>
2024-11-27 18:20:47 +0100

    Add function to return state of a persisted order

--- common/src/main/java/ch/hslu/swda/common/database/PersistedOrderDAO.java
+++ common/src/main/java/ch/hslu/swda/common/database/PersistedOrderDAO.java
@@ -1,8 +1,13 @@
 package ch.hslu.swda.common.database;
 
+import ch.hslu.swda.common.entities.Order;
 import ch.hslu.swda.common.entities.PersistedOrder;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 import static com.mongodb.client.model.Filters.eq;
@@ -18,5 +23,15 @@ public class PersistedOrderDAO extends GenericDAO<PersistedOrder> {
 
     public PersistedOrder findByTrackingId(UUID trackingId) {
         return collection.find(eq("orderItems.trackingId", trackingId)).first();
+    }
+
+    public Order.StatusEnum getOrderStatus(UUID orderId) {
+        PersistedOrder order = collection.find(eq("orderId", orderId)).first();
+
+        if (order != null) {
+            return order.getStatus();
+        }
+
+        return null;
     }
 }
