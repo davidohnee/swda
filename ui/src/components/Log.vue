baf217f4fea255d117b089a930e7548e8c5a66a3
David Dias Horta <david.diashorta@stud.hslu.ch>
2024-12-13 09:17:56 +0100

    log: add styling

--- ui/src/components/Log.vue
+++ ui/src/components/Log.vue
@@ -24,3 +24,32 @@
         </div>
     </div>
 </template>
+<style scoped>
+    .log {
+        display: grid;
+        grid-template-columns: 25ch 7ch 1fr;
+        gap: 0.5rem;
+        align-items: center;
+        font-size: 1rem;
+
+        &:not(:last-child) {
+            border-bottom: 1px solid #ccc;
+        }
+    }
+
+    code {
+        font-size: 1rem;
+    }
+
+    .level.INFO {
+        color: var(--accent);
+    }
+
+    .level.WARNING {
+        color: var(--yellow);
+    }
+
+    .level.ERROR {
+        color: var(--red);
+    }
+</style>
